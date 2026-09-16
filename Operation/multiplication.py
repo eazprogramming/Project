5aad4f1a3a90aa7faa4d997dd8f9d6bd80b51aaa
eazprogramming <eaza1014@gmail.com>
2026-09-16 09:47:38 +0800

    Add multiplication calculator in multiplication.py

---
+++ Operation/multiplication.py
@@ -0,0 +1,8 @@
+# Multiplication Calculator
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+result = num1 * num2
+
+print("The answer is:", result)
